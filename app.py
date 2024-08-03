85bf166ab7fed4c4bd57e4b42b5efb20237a1894
Shantanu mukherjee <130738064+compilation-error05@users.noreply.github.com>
2024-08-04 02:23:23 +0530

    Updated few of the glitches

--- app.py
+++ app.py
@@ -23,11 +23,11 @@ st.title('IPL First Innings Score Prediction')
 # User inputs
 bat_team = st.selectbox('Select the batting team', sorted(teams))
 bowl_team = st.selectbox('Select the bowling team', sorted(teams))
-overs = st.number_input('Enter the number of overs bowled', min_value=5.0, max_value=20.0, step=0.1)
-runs = st.number_input('Enter the number of runs scored', min_value=0)
+overs = st.number_input('Enter the number of overs bowled', min_value=5.0, max_value=19.4, step=0.1)
+runs = st.number_input('Enter the number of runs scored', min_value=0,max_value=266)
 wickets = st.number_input('Enter the number of wickets lost', min_value=0, max_value=10)
-runs_last_5 = st.number_input('Enter the runs scored in the last 5 overs', min_value=0)
-wickets_last_5 = st.number_input('Enter the wickets lost in the last 5 overs', min_value=0)
+runs_last_5 = st.number_input('Enter the runs scored in the last 5 overs', min_value=0,max_value=110)
+#wickets_last_5 = st.number_input('Enter the wickets lost in the last 5 overs', min_value=0)
 
 # Convert categorical variables using OneHotEncoding
 input_data = {
@@ -35,7 +35,7 @@ input_data = {
     'runs': runs,
     'wickets': wickets,
     'runs_last_5': runs_last_5,
-    'wickets_last_5': wickets_last_5
+    #'wickets_last_5': wickets_last_5
 }
 
 for team in teams:
